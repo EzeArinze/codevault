67c14041fcbc39c3293efe752668dc7a3c06a486
Arinze-Eze <emmanuelrinz@gmail.com>
2025-06-12 00:01:33 +0100

    link on the ai generate to go back to dashboard

--- app/(codevault)/dashboard/generate/page.tsx
+++ app/(codevault)/dashboard/generate/page.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import GenerateInput from "@/components/generate-code/generate-input";
-// import { getServerSession } from "@/utils/actions/get-server-session";
-// import { redirect } from "next/navigation";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
 
 async function AiGeneratePage() {
-  // const { session } = await getServerSession();
-
-  // if (!session) {
-  //   redirect("/sign-in");
-  // }
-
   return (
     <div className="flex min-h-[92vh] w-full gap-6 p-4">
       {/* Sidebar */}
-      <aside className="bg-muted/50 rounded-xl w-full max-w-xs p-4 md:flex flex-col hidden"></aside>
+      <aside className="bg-muted/50 rounded-xl w-full max-w-xs p-4 md:flex flex-col hidden">
+        <Link
+          href={"/dashboard"}
+          className={`${buttonVariants({ variant: "secondary" })} bg-primary/10 flex items-center`}
+        >
+          <ArrowLeft />
+          <span>dashbaord</span>
+        </Link>
+      </aside>
       {/* Main Content */}
       <main className="flex-1 bg-muted/50 rounded-xl p-6 flex flex-col justify-end">
         {/* Main content goes here */}
